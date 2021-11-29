972468b082164f10e7c65a5dc0271dd9b161fcf5
Luanbs <luankraika@hotmail.com>
2021-11-29 15:15:10 -0300

    exercicios techdive

--- src/Exercicios/Semana4Ex6.java
+++ src/Exercicios/Semana4Ex6.java
@@ -16,24 +16,23 @@ public class Semana4Ex6 {
         int diaAtual = Integer.parseInt(dataAtual.substring(0, 2));
         int mesAtual = Integer.parseInt(dataAtual.substring(3, 5));
         int anoAtual = Integer.parseInt(dataAtual.substring(6, 10));
-        String dataDeNascimento = null;
-        do {
-            System.out.println("digite sua data de nascimento no formato dd/mm/aaaa");
+
+        System.out.println("digite sua data de nascimento no formato dd/mm/aaaa");
 
 
-            dataDeNascimento = scanner.nextLine();
-            int diaNasc = Integer.parseInt(dataDeNascimento.substring(0, 2));
-            int mesNasc = Integer.parseInt(dataDeNascimento.substring(3, 5));
-            int anoNasc = Integer.parseInt(dataDeNascimento.substring(6, 10));
+        String dataDeNascimento = scanner.nextLine();
+        int diaNasc = Integer.parseInt(dataDeNascimento.substring(0, 2));
+        int mesNasc = Integer.parseInt(dataDeNascimento.substring(3, 5));
+        int anoNasc = Integer.parseInt(dataDeNascimento.substring(6, 10));
 
-            if (mesAtual > mesNasc || mesAtual == mesNasc && diaAtual >= diaNasc) {
-                idade = anoAtual - anoNasc;
-                System.out.println("Você já fez aniversário esse ano e sua idade é: " + idade);
-            } else {
-                idade = anoAtual - anoNasc - 1;
-                System.out.println("Você ainda não fez aniversário esse ano e sua idade é: " + idade);
-            }
-            System.out.println(idade >= 18 ? "Você é maior de idade" : "Você é menor de idade");
-        }while (dataDeNascimento !="-1");
+        if (mesAtual > mesNasc || mesAtual == mesNasc && diaAtual >= diaNasc) {
+            idade = anoAtual - anoNasc;
+            System.out.println("Você já fez aniversário esse ano e sua idade é: " + idade);
+        } else {
+            idade = anoAtual - anoNasc - 1;
+            System.out.println("Você ainda não fez aniversário esse ano e sua idade é: " + idade);
+        }
+        System.out.println(idade >= 18 ? "Você é maior de idade" : "Você é menor de idade");
+
     }
 }
--- src/Exercicios/Semana4Ex7.java
+++ src/Exercicios/Semana4Ex7.java
@@ -1,7 +1,19 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Semana4Ex7 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite a nota 1");
+        double nota1 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Digite a nota 2");
+        double nota2 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Digite a nota 3");
+        double nota3 = Double.parseDouble(scanner.nextLine());
+
+        double mediaFinal = (nota1+nota2+nota3)/3 ;
+        System.out.printf("Sua média final é %.2f", mediaFinal);
     }
 }
