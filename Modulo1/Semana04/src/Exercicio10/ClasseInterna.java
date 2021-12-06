badcd8b507aedf7b72f15188a5a7fc04fd12f63c
Luanbs <luankraika@hotmail.com>
2021-12-05 22:01:40 -0300

    exercicios feitos

--- Modulo1/Semana04/src/Exercicio10/ClasseInterna.java
+++ Modulo1/Semana04/src/Exercicio10/ClasseInterna.java
@@ -1,2 +1,24 @@
-package Exercicio10;public class ClasseInterna {
+package Exercicio10;
+
+
+public class ClasseInterna {
+    public class NotStatic {
+       public NotStatic(){
+           System.out.println("NotStatic instanciada");
+        }
+    }
+
+    public static class ClasseStatic{
+        public ClasseStatic(){
+            System.out.println("Static instanciada");
+        }
+    }
+
+    public static void main(String[] args) {
+        ClasseStatic statiClass = new ClasseStatic();
+        NotStatic in = new ClasseInterna().new NotStatic();
+
+    }
 }
+
+
--- Modulo1/Semana04/src/Exercicio9/Ordem.java
+++ Modulo1/Semana04/src/Exercicio9/Ordem.java
@@ -1,2 +1,19 @@
-package Exercicio9;public class Ordem {
+package Exercicio9;
+
+public class Ordem {
+    static {
+        System.out.println("dentro do bloco static");
+    }
+
+    {
+        System.out.println("dentro do bloco de instancia");
+    }
+
+    Ordem() {
+        System.out.println("dentro do construtor");
+    }
+
+    public static void main(String[] args) {
+        Ordem inicializacao = new Ordem();
+    }
 }
